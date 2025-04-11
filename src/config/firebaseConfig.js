9c4de456a9786d2951bd1370f68581c1c4373620
Lucca Silva Oliveira <146121473+luccasilva2@users.noreply.github.com>
2025-04-11 09:48:35 -0300

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -7,13 +7,13 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 // Configuração do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyA9UpV7sQ06GJdQNf7_ZjoL8t3hy7jpzgU",
-  authDomain: "griffi-2000.firebaseapp.com",
-  projectId: "griffi-2000",
-  storageBucket: "griffi-2000.appspot.com",  // Corrigido para o domínio correto
-  messagingSenderId: "470602799587",
-  appId: "1:470602799587:web:b15e92a13f7ac5ae6568d6",
-  measurementId: "G-D3LD9MX02B"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",  // Corrigido para o domínio correto
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Verifique se o Firebase já foi inicializado
